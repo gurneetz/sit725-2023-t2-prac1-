26bbcf6f3ba08fe2a2e7dfc79cb0ae649839a376
gurneetz <139436124+gurneetz@users.noreply.github.com>
2023-07-16 21:55:32 +1000

    Delete 1.js

--- 1.js
+++
@@ -1,7 +0,0 @@
-function RandomMessage() {
-  var messages = ["Hello !", "Welcome to my webpage", "Have a good day!","Enjoy your life!", "Thanks for visit"];
-var i = Math.floor(Math.random() * messages.length);
- var m = messages[i];
-    
-document.getElementById("message").innerHTML = m; }
-  
